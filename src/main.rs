876bcae4ec5dd3aaed67e44a85a7365fa7406904
Sergey Ukolov <zezic51@yandex.ru>
2023-08-11 22:44:41 +0300

    Work with RefPrinter to load stringz.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    fs,
-    io::Read, time::Instant, path::Path, env,
-};
+use std::{env, fs, io::Read, path::Path, time::Instant};
 
 use anyhow::{anyhow, Result};
 
@@ -10,7 +7,8 @@ use krakatau2::{
     file_output_util::Writer,
     lib::{
         assemble,
-        classfile::{self, code::Instr, parse::Class},
+        disassemble::refprinter::{self, RefOrString, ConstData, SingleTag, UtfData},
+        classfile::{self, code::Instr, parse::Class, attrs::AttrBody},
         AssemblerOptions, DisassemblerOptions, ParserOptions,
     },
     zip,
@@ -46,7 +44,9 @@ fn main() -> Result<()> {
         class_buf.reserve(file.size() as usize);
         file.read_to_end(&mut class_buf)?;
 
-        classes.push((name, patch_data(&class_buf)?));
+        let patched = patch_data(&name, &class_buf)?;
+
+        classes.push((name, patched));
     }
 
     bar.finish();
@@ -68,9 +68,62 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+fn trace_class(class: &Class<'_>) {
+    let mut bstable = None;
+    let mut inner_classes = None;
+    for attr in &class.attrs {
+        use AttrBody::*;
+        match &attr.body {
+            BootstrapMethods(v) => bstable = Some(v.as_ref()),
+            InnerClasses(v) => inner_classes = Some(v.as_ref()),
+            _ => {}
+        }
+    }
+
+    let rp = refprinter::RefPrinter::new(true, &class.cp, bstable, inner_classes);
+    // dbg!(&rp);
+
+    let method = class.methods.iter().skip(1).next();
+    let Some(method) = method else { return; };
+
+    let Some(attr) = method.attrs.first() else { return; };
+    let classfile::attrs::AttrBody::Code((code_1, _code_2)) = &attr.body else { return; };
+    let bytecode = &code_1.bytecode;
+    for (pos, ix) in &bytecode.0 {
+        // println!("IX: {:?}", ix);
+        if let Instr::Ldc(pool_idx) = ix {
+            // let item = class.cp.0.get(*pool_idx as usize);
+            // let item = rp.ldc(*pool_idx as u16);
+            let Some(const_line) = rp.cpool.get(*pool_idx as usize) else { continue; };
+            let ConstData::Single(SingleTag::String, idx) = const_line.data else { continue; };
+            let Some(const_line) = rp.cpool.get(idx as usize) else { continue; };
+            let ConstData::Utf8(utf_data) = &const_line.data else { continue; };
+            println!("LDC: {}", utf_data.s);
+            // match item {
+            //     RefOrString::Raw(idx) => {},
+            //     RefOrString::Sym(idx) => {},
+            //     RefOrString::RawBs(idx) => {},
+            //     RefOrString::Str(str_lit) => {
+            //         println!("STR LIT: {}", str_lit);
+            //     },
+            // };
+            // println!("CP Item: {:?}", item);
+            // if let Some(item) = class.cp.utf8(*pool_idx as u16) {
+            //     if let Ok(text) = std::str::from_utf8(item) {
+            //         println!("CP Item: {}", text);
+            //     }
+            // }
+        }
+    }
+}
+
 fn reasm(class: &Class<'_>) -> Result<Vec<u8>> {
     let mut out = Vec::new();
-    krakatau2::lib::disassemble::disassemble(&mut out, &class, DisassemblerOptions { roundtrip: true })?;
+    krakatau2::lib::disassemble::disassemble(
+        &mut out,
+        &class,
+        DisassemblerOptions { roundtrip: true },
+    )?;
 
     let source = std::str::from_utf8(&out)?;
     let mut assembled =
@@ -80,7 +133,7 @@ fn reasm(class: &Class<'_>) -> Result<Vec<u8>> {
     Ok(data)
 }
 
-fn patch_data(data: &[u8]) -> Result<Vec<u8>> {
+fn patch_data(name: &str, data: &[u8]) -> Result<Vec<u8>> {
     let mut class = classfile::parse(
         &data,
         ParserOptions {
@@ -88,6 +141,10 @@ fn patch_data(data: &[u8]) -> Result<Vec<u8>> {
         },
     )
     .map_err(|err| anyhow!("Parse: {:?}", err))?;
+
+    if name.ends_with("kek.class") {
+        trace_class(&class);
+    }
 
     patch_class(&mut class);
 
@@ -103,9 +160,13 @@ fn patch_class(class: &mut Class<'_>) {
         for (pos, ix) in bytecode.0.drain(..) {
             new_bytecode.push((pos, ix));
             let len = new_bytecode.len();
-            if len < 3 { continue; }
+            if len < 3 {
+                continue;
+            }
             let mut ixs = &mut new_bytecode[len - 3..];
-            if ixs.len() != 3 { continue; }
+            if ixs.len() != 3 {
+                continue;
+            }
             if let [(_, ix), (_, Instr::Sipush(5000)), (_, Instr::IfIcmple(_))] = &mut ixs {
                 println!("Patching integrity check");
                 *ix = Instr::Sipush(0);
