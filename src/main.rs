639e12cf2e846eaea7b1abf3979a399095ad6728
Sergey Ukolov <zezic51@yandex.ru>
2023-08-19 13:29:00 +0300

    Output current theme.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, io::Read, path::Path, time::Instant, collections::HashSet};
+use std::{env, fs, io::Read, path::Path, time::Instant};
 
 use anyhow::{anyhow, Result};
 
@@ -14,7 +14,7 @@ use krakatau2::{
             parse::Class, cpool::{ConstPool, Const, BStr},
         },
         disassemble::refprinter::{
-            self, ConstData, FmimTag, RefOrString, RefPrinter, SingleTag, UtfData,
+            self, ConstData, FmimTag, RefPrinter, SingleTag, PrimTag,
         },
         AssemblerOptions, DisassemblerOptions, ParserOptions,
     },
@@ -49,6 +49,8 @@ fn main() -> Result<()> {
         find_rgb_method_in_data(&class_buf).unwrap()
     };
 
+    let mut html = String::new();
+
     for i in 0..zip.len() {
         progress_bar.inc(1);
         let mut file = zip.by_index(i)?;
@@ -63,7 +65,7 @@ fn main() -> Result<()> {
         file.read_to_end(&mut class_buf)?;
 
         if name.ends_with("dsj.class") || name.ends_with("oMz.class") || name.ends_with("theme/kX3.class") {
-            let patched = patch_data(&name, &class_buf, &rgb_method)?;
+            let patched = patch_data(&name, &class_buf, &rgb_method, &mut html)?;
             classes.push((name, patched));
         } else {
             classes.push((name, class_buf.clone()));
@@ -85,6 +87,8 @@ fn main() -> Result<()> {
 
     let dur = Instant::now().duration_since(now);
     println!("Writed: {:?}", dur);
+
+    fs::write("theme.html", &html).expect("Unable to write theme file");
 
     Ok(())
 }
@@ -132,6 +136,12 @@ fn find_utf_ldc(rp: &RefPrinter<'_>, id: u16) -> Option<String> {
     let const_line = rp.cpool.get(idx as usize)?;
     let ConstData::Utf8(utf_data) = &const_line.data else { return None; };
     return Some(utf_data.s.to_string())
+}
+
+fn find_float_ldc(rp: &RefPrinter<'_>, id: u16) -> Option<f32> {
+    let const_line = rp.cpool.get(id as usize)?;
+    let ConstData::Prim(PrimTag::Float, float_str) = &const_line.data else { return None; };
+    float_str.trim_end_matches("f").parse::<f32>().ok()
 }
 
 fn init_refprinter<'a>(cp: &ConstPool<'a>, attrs: &'a [Attribute<'a>]) -> RefPrinter<'a> {
@@ -191,8 +201,79 @@ fn find_rgb_method(class: &Class<'_>) -> Option<(MethodId, MethodDescription)> {
     None
 }
 
-fn randomize_class<'a>(name: &str, class: &mut Class<'a>, method_idx: usize, rgb_method_desc: &'a MethodDescription) {
+#[derive(Debug)]
+enum Color {
+    Rgbu(u8, u8, u8),
+    HsvfAdjustment(f32, f32, f32),
+    Rgbau(u8, u8, u8, u8),
+    Grayscale(u8),
+}
+
+#[derive(Debug)]
+struct ColorDef {
+    name: String,
+    color: Color,
+}
+
+impl ColorDef {
+    fn as_html(&self) -> String {
+        let color_style = match self.color {
+            Color::Rgbu(r, g, b) => format!("rgb({r}, {g}, {b})"),
+            Color::HsvfAdjustment(h, s, v) => format!("gray"),
+            Color::Rgbau(r, g, b, a) => {
+                let a_f = a as f32 / 255.0;
+                format!("rgba({r}, {g}, {b}, {a_f})")
+            },
+            Color::Grayscale(v) => format!("rgb({v}, {v}, {v})"),
+        };
+        let name = &self.name;
+        let stripes = "background: repeating-linear-gradient(45deg, #000000, #000000 10px, #ffffff 10px, #ffffff 20px);";
+        format!("<div style='display: flex;'><div style='{stripes}'><div style='background-color: {color_style}; height: 40px; width: 80px;'></div></div>{name}</div>")
+    }
+}
+
+#[derive(Eq, PartialEq)]
+enum ColorMethod {
+    Rgbu,
+    HsvfAdjustment,
+    Rgbau,
+    Grayscale,
+}
+
+fn instr_to_float(instr: Instr, rp: &RefPrinter<'_>) -> f32 {
+    match instr {
+        Instr::Ldc(id) => {
+            find_float_ldc(&rp, id as u16).unwrap()
+        }
+        Instr::Fconst0 => 0.0,
+        Instr::Fconst1 => 1.0,
+        Instr::Fconst2 => 2.0,
+        _ => unreachable!("Unexpected IX for float")
+    }
+}
+
+fn instr_to_u8(instr: &Instr) -> u8 {
+    match instr {
+        Instr::Iconst0 => 0,
+        Instr::Iconst1 => 1,
+        Instr::Iconst2 => 2,
+        Instr::Iconst3 => 3,
+        Instr::Iconst4 => 4,
+        Instr::Iconst5 => 5,
+        Instr::Bipush(num) => *num as u8,
+        Instr::Sipush(num) => *num as u8,
+        _ => unreachable!("Unexpected IX for u8")
+    }
+}
+
+fn randomize_class<'a>(name: &str, class: &mut Class<'a>, method_idx: usize, rgb_method_desc: &'a MethodDescription) -> Result<Vec<ColorDef>> {
     println!("Randomizing {}", name);
+    let mut color_defs = vec![];
+
+    // println!("CP:");
+    // for (idx, x) in class.cp.0.iter().enumerate() {
+    //     println!("{}: {:?}", idx, x);
+    // }
 
     let (rgb_method_id, rgb_method_desc) = match find_rgb_method(class) {
         Some(met) => met,
@@ -223,20 +304,20 @@ fn randomize_class<'a>(name: &str, class: &mut Class<'a>, method_idx: usize, rgb
 
     println!("RGB METHOD: {} {:?}", rgb_method_id, rgb_method_desc);
 
-    const COLOR_DEFINE_SIGS: &[(&str, usize)] = &[
-        ("(Ljava/lang/String;I)", 1),
-        ("(Ljava/lang/String;III)", 3),
-        // ("(Ljava/lang/String;IIII)", 4),
-        ("(Ljava/lang/String;FFF)", 3),
+    const COLOR_DEFINE_SIGS: &[(&str, usize, ColorMethod)] = &[
+        ("(Ljava/lang/String;I)", 1, ColorMethod::Grayscale),
+        ("(Ljava/lang/String;III)", 3, ColorMethod::Rgbu),
+        ("(Ljava/lang/String;IIII)", 4, ColorMethod::Rgbau),
+        ("(Ljava/lang/String;FFF)", 3, ColorMethod::HsvfAdjustment),
     ];
 
     let rp = init_refprinter(&class.cp, &class.attrs);
 
     let method = class.methods.iter_mut().skip(method_idx).next();
-    let Some(method) = method else { return; };
+    let Some(method) = method else { return Err(anyhow!("No method at offset {}", method_idx)); };
 
-    let Some(attr) = method.attrs.first_mut() else { return; };
-    let classfile::attrs::AttrBody::Code((code_1, _code_2)) = &mut attr.body else { return; };
+    let Some(attr) = method.attrs.first_mut() else { return Err(anyhow!("No first attr in method")); };
+    let classfile::attrs::AttrBody::Code((code_1, _code_2)) = &mut attr.body else { return Err(anyhow!("Attr body is not Code")); };
     let bytecode = &mut code_1.bytecode;
 
     let mut new_bytecode: Vec<(Pos, Instr)> = vec![];
@@ -244,12 +325,10 @@ fn randomize_class<'a>(name: &str, class: &mut Class<'a>, method_idx: usize, rgb
     let mut pos_gen = 0;
 
     for (_pos, ix) in bytecode.0.drain(..) {
-        // println!("POS: {:?} IX: {:?}", pos, ix);
         let should_replace = match &ix {
             Instr::Invokevirtual(method_id) => {
                 if let Some(method_descr) = find_method_description(&rp, *method_id) {
-                    // println!("{:?}", method_descr);
-                    COLOR_DEFINE_SIGS.iter().find_map(|(sig, color_args)| method_descr.signature.starts_with(sig).then_some(color_args))
+                    COLOR_DEFINE_SIGS.iter().find_map(|(sig, color_args, color_method)| method_descr.signature.starts_with(sig).then_some((color_args, color_method)))
                 } else {
                     None
                 }
@@ -257,32 +336,77 @@ fn randomize_class<'a>(name: &str, class: &mut Class<'a>, method_idx: usize, rgb
             _ => None,
         };
 
-        if let Some(color_args) = should_replace {
-            for _ in 0..*color_args {
-                new_bytecode.pop().unwrap();
+        if let Some((color_args, color_method)) = should_replace {
+            let has_alpha = *color_method == ColorMethod::Rgbau;
+            let alpha_ix = if has_alpha {
+                Some(new_bytecode.pop().unwrap().1)
+            } else {
+                None
+            };
+
+            let mut bgr = vec![];
+
+            for _ in 0..*color_args - if has_alpha { 1 } else { 0 } {
+                bgr.push(new_bytecode.pop().unwrap().1);
             }
+
+            let maybe_ldc = new_bytecode.last().unwrap();
+            if let Instr::Ldc(id) = maybe_ldc.1 {
+                let ldc = find_utf_ldc(&rp, id as u16).unwrap();
+                // println!("LDC {:?} BGR {:?} ALPHA {:?}", ldc, bgr, alpha_ix);
+                let color_def = ColorDef {
+                    name: ldc,
+                    color: match color_method {
+                        ColorMethod::Rgbu => {
+                            let r = instr_to_u8(&bgr.pop().unwrap());
+                            let g = instr_to_u8(&bgr.pop().unwrap());
+                            let b = instr_to_u8(&bgr.pop().unwrap());
+                            Color::Rgbu(r, g, b)
+                        },
+                        ColorMethod::HsvfAdjustment => {
+                            let h = instr_to_float(bgr.pop().unwrap(), &rp);
+                            let s = instr_to_float(bgr.pop().unwrap(), &rp);
+                            let v = instr_to_float(bgr.pop().unwrap(), &rp);
+                            Color::HsvfAdjustment(h, s, v)
+                        },
+                        ColorMethod::Rgbau => {
+                            let r = instr_to_u8(&bgr.pop().unwrap());
+                            let g = instr_to_u8(&bgr.pop().unwrap());
+                            let b = instr_to_u8(&bgr.pop().unwrap());
+                            let ix = alpha_ix.as_ref().unwrap();
+                            let a = instr_to_u8(ix);
+                            Color::Rgbau(r, g, b, a)
+                        },
+                        ColorMethod::Grayscale => {
+                            let v = instr_to_u8(&bgr.pop().unwrap());
+                            Color::Grayscale(v)
+                        }
+                    }
+                };
+                color_defs.push(color_def);
+            }
+
             for _ in 0..3 {
                 let rn: u8 = rand::random();
                 let new = (
                     Pos(pos_gen),
                     Instr::Sipush(rn as i16)
                 );
-                // println!("NEW: {:?}", new);
                 new_bytecode.push(new);
                 pos_gen += 1;
             }
-            // let last_label = new_bytecode.last().unwrap().0.0;
-            // let this_label = pos.0;
-            new_bytecode.push((Pos(pos_gen), Instr::Invokevirtual(rgb_method_id)));
+            if has_alpha {
+                new_bytecode.push((Pos(pos_gen), alpha_ix.unwrap()));
+                pos_gen += 1;
+                new_bytecode.push((Pos(pos_gen), ix));
+            } else {
+                new_bytecode.push((Pos(pos_gen), Instr::Invokevirtual(rgb_method_id)));
+            };
             pos_gen += 1;
-            // println!("---");
         } else {
             new_bytecode.push((Pos(pos_gen), ix));
             pos_gen += 1;
         }
-
-        // if let Instr::Ldc(pool_idx) = ix {
-        // }
     }
     bytecode.0 = new_bytecode;
 
@@ -290,6 +414,8 @@ fn randomize_class<'a>(name: &str, class: &mut Class<'a>, method_idx: usize, rgb
         let classfile::attrs::AttrBody::LineNumberTable(table) = &mut attr.body else { continue; };
         table.clear();
     }
+
+    Ok(color_defs)
 }
 
 fn reasm(class: &Class<'_>) -> Result<Vec<u8>> {
@@ -308,7 +434,7 @@ fn reasm(class: &Class<'_>) -> Result<Vec<u8>> {
     Ok(data)
 }
 
-fn patch_data(name: &str, data: &[u8], rgb_method_desc: &MethodDescription) -> Result<Vec<u8>> {
+fn patch_data(name: &str, data: &[u8], rgb_method_desc: &MethodDescription, html: &mut String) -> Result<Vec<u8>> {
     let mut class = classfile::parse(
         &data,
         ParserOptions {
@@ -319,7 +445,11 @@ fn patch_data(name: &str, data: &[u8], rgb_method_desc: &MethodDescription) -> R
 
     if name.ends_with("dsj.class") || name.ends_with("kX3.class") {
         let skip = if name.ends_with("dsj.class") { 1 } else if name.ends_with("kX3.class") { 4 } else { 0 };
-        randomize_class(name, &mut class, skip, rgb_method_desc);
+        let color_defs = randomize_class(name, &mut class, skip, rgb_method_desc).unwrap();
+        for def in color_defs {
+            let def_html = def.as_html();
+            html.push_str(&format!("{def_html}\n"));
+        }
         Ok(reasm(&class)?)
     } else if name.ends_with("oMz.class") {
         patch_class(name, &mut class);
@@ -327,7 +457,6 @@ fn patch_data(name: &str, data: &[u8], rgb_method_desc: &MethodDescription) -> R
     } else {
         panic!("raositenars");
     }
-
 }
 
 fn patch_class(name: &str, class: &mut Class<'_>) {
