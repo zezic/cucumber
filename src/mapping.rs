cf007e267e54f5b2ad890c5ea7c0c9a76b24121f
Sergey Ukolov <zezic51@yandex.ru>
2023-08-20 13:08:31 +0300

    Describe some Bitwig colors.

--- src/mapping.rs
+++ src/mapping.rs
@@ -1,3 +1,60 @@
 pub const RAW_MAPPING: &[(&str, &str)] = &[
-    ("Window background", "SurfaceArea"),
+    // ("Default text", "AbletonColor"), // ---
+    // ("Window background", "AbletonColor"), // Outer void, main Bitwig window body color, preferences BG
+    // ("Panel body", "AbletonColor"), // Top panel, non-selected track headers, scene headers, device BG
+    // ("Panel stroke", "AbletonColor"), // Non-selected GUI section, preferences window contour
+    // ("Active Panel stroke", "AbletonColor"), // Sselected GUI section
+    // ("Hole (dark)", "AbletonColor"), // Browser void, device area void, track headers and clip launcher void 
+    // ("Hole (medium)", "AbletonColor"), // Side-browser heading, empty clip launcher cells, clip launcher/arranger switch selected button, device mini-stack BG, track inspector tab content bg, popup browser panels, device knob sections, VU meter BG, 
+    // ("Hole (light)", "AbletonColor"), // Secondary areas - scrollbar BG, modulator slots, device control areas
+    // ("Shadow", "AbletonColor"), // Used in linear gradient for scrollable area fadeouts
+    // ("Selected Dashboard Tree", "AbletonColor"), // Selected entry background in dashboard (package man, recent projects, etc)
+    // ("Selected Panel body", "AbletonColor"), // Selected track header
+    // ("Selected Panel stroke", "AbletonColor"), // ---
+    // ("Selected Panel stroke (standby)", "AbletonColor"), // ---
+    // ("Popup insert", "AbletonColor"), // Popup browser contour + contour of device it replaces
+    // ("Button stroke", "AbletonColor"), // Button contour
+    // ("Light button stroke", "AbletonColor"), // ---
+    // ("Rubber button stroke", "AbletonColor"), // ---
+    // ("Button background", "AbletonColor"), // Literally, button background (non-highlighted)
+    // ("Pressed button background", "AbletonColor"), // When non-highlighted button pressed by cursor
+    // ("Checkbox background", "AbletonColor"), // Non-highlighted checkbox background (found in prefs)
+    // ("Button in tree background", "AbletonColor"), // ---
+    // ("View button background", "AbletonColor"), // Non-highlighted button in arranger statusbar (grid snap, track header view options)
+    // ("Pressed view button background", "AbletonColor"), // Highlighted button in arranger statusbar (track header view options, etc)
+    // ("Inverted Selected Borderless Button background", "AbletonColor"), // ---
+    // ("Selected borderless button background", "AbletonColor"), // Controller prefs options buttons
+    // ("Pressed borderless button background", "AbletonColor"), // Controller prefs options buttons
+    // ("Rubber highlight button stroke", "AbletonColor"), // ---
+    // ("Rubber Button Emboss Highlight", "AbletonColor"), // ---
+    // ("Selection", "AbletonColor"), // Selected text BG
+    // ("Standby selection", "AbletonColor"), // Selected text BG (inactive window)
+    // ("On", "AbletonColor"), // Main accent color (not only buttons, but also track faders and some accent labels)
+    // ("On (subtle)", "AbletonColor"), // ---
+    // ("On (subtler)", "AbletonColor"), // ---
+    // ("Pressed On", "AbletonColor"), // Pressed highglighted button
+    // ("Implicit On (subtle)", "AbletonColor"), // ---
+    // ("Hitech on", "AbletonColor"), // Top display text (BPM, bar), some selected values in prefs, badges background, active browser icon in device, selected browser filter FG, play marker
+    // ("Hitech background", "AbletonColor"), // ---
+    // ("Mapping", "AbletonColor"), // Various controller mapping highlights
+    // ("Notification Background", "AbletonColor"), // Notification BG
+    // ("Notification Normal", "AbletonColor"), // Notification FG
+    // ("Notification Error", "AbletonColor"), // Notification FG (Error)
+    // ("Warning", "AbletonColor"), // Probably, some warning highlight (not sure)
+    // ("Automation Color", "AbletonColor"), // ---
+    // ("User Automation Override Color", "AbletonColor"), // Small dot at control and icon at top display
+    // ("Modulation Mapping Color", "AbletonColor"), // Overlay for modulatable controls, positive modulation amount, modulator arrow
+    // ("Modulation Mapping Background Color", "AbletonColor"), // Background higlight in inspector modulations list when hovering over modulator arrow
+    // ("Modulation Mapping Color (subtractive)", "AbletonColor"), // Literally
+    // ("Modulation Mapping Color (polyphonic)", "AbletonColor"), // Literally
+    // ("Modulation Mapping Background (polyphonic)", "AbletonColor"), // Literally
+    // ("Menu background", "AbletonColor"), // Popup menu background
+    // ("Menu stroke", "AbletonColor"), // Popup menu contour
+    // ("Menu text", "AbletonColor"), // Popup menu FG
+    // ("Menu Icon", "AbletonColor"), // Popup menu icons
+    // ("Menu description text", "AbletonColor"), // ---
+    // ("Menu separator", "AbletonColor"), // Popup menu separators
+    // ----------------------------------------
+    // ("Display Background", "AbletonColor"), // Some dark text inputs, slider holes, sampler waveform display BG, top display BG, VU meters BG for tracks, many device displays (Amp, etc), mixer channel text notes area BG, clip launcher clips separators, shortcuts prefs BG and other prefs fields BGs
+    // ("Display Waveform", "AbletonColor"), // Waveform in sampler
 ];
